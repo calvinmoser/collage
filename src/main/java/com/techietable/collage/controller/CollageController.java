0d443e52cd1dc463fc5d50f77be3abc637c65282
Calvin Moser <cmoser@techietable.com>
2026-05-06 17:59:12 -0400

    Serve up subpages properly.

--- src/main/java/com/techietable/collage/controller/CollageController.java
+++ src/main/java/com/techietable/collage/controller/CollageController.java
@@ -1,6 +1,8 @@
 package com.techietable.collage.controller;
 
 import com.techietable.collage.service.BackgroundCache;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,16 @@ public class CollageController {
 
     @Autowired
     private BackgroundCache backgroundCache;
+
+    @GetMapping({"/tutoring", "/tutoring/"})
+    public void tutoring(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        req.getRequestDispatcher("/tutoring/index.html").forward(req, res);
+    }
+
+    @GetMapping({"/contact", "/contact/"})
+    public void contact(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        req.getRequestDispatcher("/contact/index.html").forward(req, res);
+    }
 
     @GetMapping("/api/background")
     public ResponseEntity<byte[]> background() throws Exception {
